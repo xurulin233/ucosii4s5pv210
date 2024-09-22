12250e71730f678ef01e7d41d9abd8cb97a20cf5
xurulin <804362478@qq.com>
2024-09-22 21:07:27 +0800

    add Semaphore

--- ucos2/app/main.c
+++ ucos2/app/main.c
@@ -10,6 +10,10 @@ OS_STK  MainTaskStk[MainTaskStkLengh];
 OS_STK	Task0Stk[Task0StkLengh];       															
 OS_STK	Task1Stk[Task1StkLengh];  
 
+//信号量
+OS_EVENT *sem;
+
+
 int main(void)
 {
 	
@@ -50,6 +54,10 @@ void MainTask(void *pdata)
     // 如果要使用统计任务，则在用户的第一个任务里需调用OSStatInit函数
     OSStatInit();																				
     // 创建用户应用程序任务
+
+	//创建信号量
+	sem = OSSemCreate(1);  
+	
     OSTaskCreate(Task0,(void *)0, &Task0Stk[Task0StkLengh - 1], Task0Prio);			
     OSTaskCreate(Task1,(void *)0, &Task1Stk[Task1StkLengh - 1], Task1Prio);
  
@@ -61,12 +69,39 @@ void MainTask(void *pdata)
     }
 }
 
+
+void delay1(void)
+{
+	volatile unsigned int i = 9000000;		// volatile 让编译器不要优化，这样才能真正的减
+	while (i--);							// 才能消耗时间，实现delay
+}
+
+
 void Task0(void *pdata)
 {		
+	INT8U err;
 	while (1)
 	{
-	    printf("Enter Task1\r\n"); 
-		OSTaskSuspend(Task0Prio);
+
+		OSSemPend(sem,0,&err);
+		if (err == OS_NO_ERR) { 
+			printf("led on\r\n");
+			led1();
+			delay1();
+			printf("led onn\r\n");
+			OSSemPost(sem); 
+			}
+		OSTimeDly(10);
+
+/*
+		printf("led on\r\n");
+		led1();
+		delay1();
+		printf("led on\r\n");
+*/	
+	
+	    //printf("Enter Task1\r\n"); 
+		//OSTaskSuspend(Task0Prio);
 	    //OSTimeDly(OS_TICKS_PER_SEC/5);
 	}
 }
@@ -75,16 +110,38 @@ void Task0(void *pdata)
 
 void Task1(void *pdata)
 {		
+	INT8U err;
 	while (1)
 	{
-	    //printf("Enter Task2\r\n");	
-		//OSTaskResume(Task0Prio); 
-		//printf("OSTaskResume Task1 end\r\n");
-	    //OSTimeDly(OS_TICKS_PER_SEC/5);
-	    printf("OSCPUUsage = %d % \r\n",OSCPUUsage);
-	    OSTimeDly(OS_TICKS_PER_SEC/200);
+
+		OSSemPend(sem, 0, &err); // 等待获取信号量 
+		if (err == OS_NO_ERR) { 
+			printf("led off \r\n");
+			led_off();
+			delay1();
+			printf("led offf \r\n");
+			OSSemPost(sem); // 释放信号量
+		} 
+		OSTimeDly(10);
+
+/*
+		printf("led off \r\n");
+		led_off();
+		delay1();
+		printf("led offf \r\n");
+		OSTimeDly(10);
+*/
+    //printf("Enter Task2\r\n");	
+	//OSTaskResume(Task0Prio); 
+	//printf("OSTaskResume Task1 end\r\n");
+    //OSTimeDly(OS_TICKS_PER_SEC/5);
+    //printf("OSCPUUsage = %d % \r\n",OSCPUUsage);
+    //OSTimeDly(OS_TICKS_PER_SEC/200);
+		
 	}
 }
+
+
 
 
 int raise(int param)
